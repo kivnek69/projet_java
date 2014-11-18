98fa4768ba079346c3a4893f15aae4da4455fb8a
kivnek69 <kivnek69@gmail.com>
2014-11-18 14:42:00 +0100

    comit

--- distributeurAutomatique/src/distributeurautomatique/index.java
+++ distributeurAutomatique/src/distributeurautomatique/index.java
@@ -14,6 +14,7 @@ public class index {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        interfaceUtilisateur intUser = new interfaceUtilisateur();
+        intUser.setVisible(true);
     }
 }
