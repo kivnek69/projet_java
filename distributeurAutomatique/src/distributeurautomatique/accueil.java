6894578839ecba1b3b264217b820b11be72dcd96
kivnek69 <kivnek69@gmail.com>
2014-11-18 16:38:44 +0100

    comit

---
+++ distributeurAutomatique/src/distributeurautomatique/accueil.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package distributeurautomatique;
+
+/**
+ *
+ * @author rafik and mohaned
+ */
+public class accueil extends javax.swing.JFrame {
+    
+    
+    public accueil(){
+        
+    }
+    
+     private void jButton5ActionPerformed(java.awt.event.ActionEvent evt) {                                         
+        
+    } 
+    
+    
+     private javax.swing.JButton jButton5;
+}
